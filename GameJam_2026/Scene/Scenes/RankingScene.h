8bdb18391caccdc622b3132cb4aee38d1740262e
riku-shinjo-k25f-bit <riku-shinjo-k25f@stu.kbc.ac.jp>
2026-02-20 15:58:19 +0900

    ランキングの描画

--- GameJam_2026/Scene/Scenes/RankingScene.h
+++ GameJam_2026/Scene/Scenes/RankingScene.h
@@ -1,11 +1,32 @@
 #pragma once
 #include"../SceneBase.h"
 
+#define  DIGIT   5
+
 class RankingScene : public SceneBase
 {
 protected:
 
 private:
+	int numbers_image[11] = {};
+
+	int high_score = 12345;
+	int second_score = 2345;
+	int third_score = 345;
+	int four_score = 45;
+	int five_score = 5;
+
+	Vector2D highscore_location[DIGIT] = {};
+	Vector2D secondscore_location[DIGIT] = {};
+	Vector2D thirdscore_location[DIGIT] = {};
+	Vector2D fourscore_location[DIGIT] = {};
+	Vector2D fivescore_location[DIGIT] = {};
+	int draw_number[DIGIT] = { 10,10,10,10,10 };
+	int draw_number2[DIGIT] = { 10,10,10,10,10 };
+	int draw_number3[DIGIT] = { 10,10,10,10,10 };
+	int draw_number4[DIGIT] = { 10,10,10,10,10 };
+	int draw_number5[DIGIT] = { 10,10,10,10,10 };
+
 
 public:
 	RankingScene();
