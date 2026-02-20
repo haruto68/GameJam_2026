96b3e0572988143d450958cd496826ded6314493
haruto68 <138412489+haruto68@users.noreply.github.com>
2026-02-20 16:00:15 +0900

    csvファイルの追加と読み書き

--- GameJam_2026/Scene/Scenes/ResultScene.h
+++ GameJam_2026/Scene/Scenes/ResultScene.h
@@ -8,6 +8,18 @@ protected:
 private:
 	int cursor_num = 0;
 
+	int high_score = 0;
+	int second_score = 0;
+	int third_score = 0;
+	int four_score = 0;
+	int five_score = 0;
+
+	int ranking[5] = {};
+
+	int score = 0;
+	int item = 0;
+	int mato[3] = {};
+
 public:
 	ResultScene();
 	~ResultScene();
